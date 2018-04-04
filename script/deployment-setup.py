f04f5ecc47fc4de77a395cf95b7e46a5295c622e
Maurice Yap <mauriceyap@hotmail.co.uk>
2018-04-04 18:33:27 +0100

    gh pages deployment

---
+++ script/deployment-setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Change _config.yml variables as appropriate for branch
+"""
+
+TRAVIS_BRANCH = "TRAVIS_BRANCH"
+MASTER = "master"
+STAGING = "staging"
+CONFIG_YML = os.path.join("..", "_config.yml")
+BASEURL = "baseurl"
+URL = "url"
+
+branch_name = os.environ[TRAVIS_BRANCH]
+
+if branch_name == MASTER:
+    pass
+    # TODO: change _config.yml to iccu.co.uk settings
+elif branch_name == STAGING:
+    with open(CONFIG_YML) as f:
+        config = yaml.load(f)
+    config[BASEURL] = "/iccu-website"
+    config[URL] = "https://imperial-cu.github.io/iccu-website"
+
+    with open(CONFIG_YML, "w") as f:
+        yaml.dump(list_doc, f)
