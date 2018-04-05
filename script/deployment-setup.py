f40ae94a4c637a81120abb5ed611a257c24c0234
Maurice Yap <mauriceyap@hotmail.co.uk>
2018-04-05 18:00:09 +0100

    muse netlify for staging

--- script/deployment-setup.py
+++ script/deployment-setup.py
@@ -9,6 +9,7 @@ import yaml
 TRAVIS_BRANCH = "TRAVIS_BRANCH"
 MASTER = "master"
 STAGING = "staging"
+GH_PAGES = "gh-pages"
 CONFIG_YML = os.path.join(".", "_config.yml")
 BASEURL = "baseurl"
 URL = "url"
@@ -18,11 +19,19 @@ branch_name = os.environ[TRAVIS_BRANCH]
 if branch_name == MASTER:
     pass
     # TODO: change _config.yml to iccu.co.uk settings
-elif branch_name == STAGING:
+elif branch_name == GH_PAGES:
     with open(CONFIG_YML) as f:
         config = yaml.load(f)
     config[BASEURL] = "/iccu-website"
     config[URL] = "https://imperial-cu.github.io/iccu-website"
+
+    with open(CONFIG_YML, "w") as f:
+        yaml.dump(config, f)
+elif branch_name == STAGING:
+    with open(CONFIG_YML) as f:
+        config = yaml.load(f)
+    config[BASEURL] = ""
+    config[URL] = ""
 
     with open(CONFIG_YML, "w") as f:
         yaml.dump(config, f)
