17e0c22842d410ea4fb7809f16223e1a1644d576
Maurice Yap <mauriceyap@hotmail.co.uk>
2018-04-27 23:12:58 +0100

    additional prose config

--- script/deployment-setup.py
+++ script/deployment-setup.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-"""
-Change _config.yml variables as appropriate for branch
-"""
 
 import os
 import yaml
@@ -9,29 +6,16 @@ import yaml
 TRAVIS_BRANCH = "TRAVIS_BRANCH"
 MASTER = "master"
 STAGING = "staging"
-GH_PAGES = "gh-pages"
 CONFIG_YML = os.path.join(".", "_config.yml")
 BASEURL = "baseurl"
 URL = "url"
 
 branch_name = os.environ[TRAVIS_BRANCH]
 
-if branch_name == MASTER:
-    pass
-    # TODO: change _config.yml to iccu.co.uk settings
-elif branch_name == GH_PAGES:
-    with open(CONFIG_YML) as f:
-        config = yaml.load(f)
-    config[BASEURL] = "/iccu-website"
-    config[URL] = "https://imperial-cu.github.io/iccu-website"
-
-    with open(CONFIG_YML, "w") as f:
-        yaml.dump(config, f)
-elif branch_name == STAGING:
+if branch_name == STAGING:
     with open(CONFIG_YML) as f:
         config = yaml.load(f)
     config[BASEURL] = ""
-    config[URL] = "https://wonderful-engelbart-98ae8d.netlify.com"
-
+    config[URL] = "https://staging.iccu.co.uk"
     with open(CONFIG_YML, "w") as f:
         yaml.dump(config, f)
